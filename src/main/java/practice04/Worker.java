9439f7589ba2b611a42e724a779ac2b305b78098
shalldi <shalldid@hotmail.com>
2019-08-22 19:58:29 +0800

    pracitce04

---
+++ src/main/java/practice04/Worker.java
@@ -0,0 +1,13 @@
+package practice04;
+
+public class Worker extends Person{
+
+	public Worker(String name, int age) {
+		super(name, age);
+	}
+
+	@Override
+	public String introduce() {
+		return String.format("My name is %s. I am %d years old. I am a Worker. I have a job.", this.getName(), this.getAge());
+	}
+}
